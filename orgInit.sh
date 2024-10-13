e9268ccbee18fba1c6617408bcc50d919774ec5f
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-13 00:05:35 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,9 +1,9 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 30 -s -w 5
-sfdx force:source:push
-sfdx force:user:password:generate
-sfdx force:user:permset:assign --permsetname Admin_de_Contratos
-sfdx force:data:bulk:upsert -s Account -f data/Cuenta.csv -i Id_Externo__c -w 3
-sfdx force:data:bulk:upsert -s Account -f data/CuentaContratosActivos.csv -i Id_Externo__c -w 3
-sfdx force:data:bulk:upsert -s Contrato__c -f data/ContratosHistoricosData2.csv -i Id_Externo__c -w 3
-sfdx force:data:bulk:upsert -s Contrato__c -f data/ContratosActivos2.csv -i Id_Externo__c -w 3
-sfdx force:org:open
+sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p contrato -e einstein.demo
+sf project deploy start
+sf demoutil user password set -p salesforce1 -g User -l User
+sf org assign permset --permsetname Admin_de_Contratos
+sf data upsert bulk -s Account -f data/Cuenta.csv -i Id_Externo__c -w 3
+sf data upsert bulk -s Account -f data/CuentaContratosActivos.csv -i Id_Externo__c -w 3
+sf data upsert bulk -s Contrato__c -f data/ContratosHistoricosData2.csv -i Id_Externo__c -w 3
+sf data upsert bulk -s Contrato__c -f data/ContratosActivos2.csv -i Id_Externo__c -w 3
+sf org open
