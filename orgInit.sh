61487004736ef40e3d68d666d38cec9e887213b2
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-28 22:42:58 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,7 +1,7 @@
 sf demoutil org create scratch -f config/project-scratch-def.json -d 5 -s -p contrato -e einstein.demo
 sf project deploy start
 sf demoutil user password set -p salesforce1 -g User -l User
-sf org assign permset --permsetname Admin_de_Contratos
+sf org assign permset -n Admin_de_Contratos
 sf data upsert bulk -s Account -f data/Cuenta.csv -i Id_Externo__c -w 3
 sf data upsert bulk -s Account -f data/CuentaContratosActivos.csv -i Id_Externo__c -w 3
 sf data upsert bulk -s Contrato__c -f data/ContratosHistoricosData2.csv -i Id_Externo__c -w 3
